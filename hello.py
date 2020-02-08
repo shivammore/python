7c0edb0dba902ae8573e8c45fccf0d5f50981b00
shivammore <shivamkm1692000@gmail.com>
2020-02-08 13:26:12 +0530

    If added

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
-print("Hello")
+print("Who are you??")
+c=input()
 
-print(10+5)
+print("Hello"+c)
+
+print("how are u feeling"+c)
+b=input()
+if c== "nice" :
+   print("it seems that you do not need my help")
+   
+else :
+   print(how can i help you)
